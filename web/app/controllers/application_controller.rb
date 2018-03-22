3db7cd9ba5dfdf2f4b09bf03c0223d80ce8001f3
Thomas Monzel <tom.monzel@gmail.com>
2018-03-22 13:59:44 +0100

    Serve ui node_module resources over get /vendor/*

--- web/app/controllers/application_controller.rb
+++ web/app/controllers/application_controller.rb
@@ -5,7 +5,13 @@ class ApplicationController < Sinatra::Base
 
   set :public_folder, "app/public"
 
+  get '/vendor/*' do
+    filename = params['splat'].first;
+    send_file File.join("node_modules", filename)
+  end
+
   get '/' do
+    # auto serve index.html
     send_file File.join(settings.public_folder, "index.html")
   end
 end
