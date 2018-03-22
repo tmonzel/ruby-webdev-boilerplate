dd54eedecfea2db15065be3d800a6329b4e32274
Thomas Monzel <tom.monzel@gmail.com>
2018-03-22 09:06:36 +0100

    Added DB_HOST to make the host resolvable for docker

--- web/api/db_config.rb
+++ web/api/db_config.rb
@@ -5,7 +5,7 @@ require_relative 'models/movie'
 
 ActiveRecord::Base.establish_connection(
   adapter: "mysql2",
-  host: "192.168.99.100",
+  host: ENV['DB_HOST'] || 'localhost',
   username: "web_user",
   password: "test",
   database: "web_data"
